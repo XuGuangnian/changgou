3c5bef05b5b095513fbe935bfa7bec51ad3ec69e
XuGuangnian <gengen.wang@outlook.com>
2021-10-14 14:44:32 +0800

    基础架构代码2

--- changgou_parent/changgou_service/changgou_service_goods/src/main/java/com/changgou/goods/controller/BrandController.java
+++ changgou_parent/changgou_service/changgou_service_goods/src/main/java/com/changgou/goods/controller/BrandController.java
@@ -1,100 +1,94 @@
 package com.changgou.goods.controller;
-
 import com.changgou.entity.PageResult;
 import com.changgou.entity.Result;
 import com.changgou.entity.StatusCode;
-import com.changgou.goods.pojo.Brand;
 import com.changgou.goods.service.BrandService;
+import com.changgou.goods.pojo.Brand;
 import com.github.pagehelper.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Map;
-
 @RestController
+@CrossOrigin
 @RequestMapping("/brand")
 public class BrandController {
-    
+
+
     @Autowired
     private BrandService brandService;
-    
+
+    /**
+     * 查询全部数据
+     * @return
+     */
     @GetMapping
-    public Result findAll() {
+    public Result findAll(){
         List<Brand> brandList = brandService.findAll();
-        return new Result(true, StatusCode.OK, "查询成功", brandList);
+        return new Result(true, StatusCode.OK,"查询成功",brandList) ;
     }
-    
+
     /***
-     * 根据ID查询品牌数据
+     * 根据ID查询数据
      * @param id
      * @return
      */
     @GetMapping("/{id}")
-    public Result findById(@PathVariable Integer id) {
+    public Result findById(@PathVariable Integer id){
         Brand brand = brandService.findById(id);
-        return new Result(true, StatusCode.OK, "查询成功", brand);
+        return new Result(true,StatusCode.OK,"查询成功",brand);
     }
-    
+
+
     /***
-     * 新增品牌数据
+     * 新增数据
      * @param brand
      * @return
      */
     @PostMapping
-    public Result add(@RequestBody Brand brand) {
+    public Result add(@RequestBody Brand brand){
         brandService.add(brand);
-        return new Result(true, StatusCode.OK, "添加成功");
+        return new Result(true,StatusCode.OK,"添加成功");
     }
-    
+
+
     /***
-     * 修改品牌数据
+     * 修改数据
      * @param brand
      * @param id
      * @return
      */
-    @PutMapping(value = "/{id}")
-    public Result update(@RequestBody Brand brand, @PathVariable Integer id) {
+    @PutMapping(value="/{id}")
+    public Result update(@RequestBody Brand brand,@PathVariable Integer id){
         brand.setId(id);
         brandService.update(brand);
-        return new Result(true, StatusCode.OK, "修改成功");
+        return new Result(true,StatusCode.OK,"修改成功");
     }
-    
+
+
     /***
      * 根据ID删除品牌数据
      * @param id
      * @return
      */
-    @DeleteMapping(value = "/{id}")
-    public Result delete(@PathVariable Integer id) {
+    @DeleteMapping(value = "/{id}" )
+    public Result delete(@PathVariable Integer id){
         brandService.delete(id);
-        return new Result(true, StatusCode.OK, "删除成功");
+        return new Result(true,StatusCode.OK,"删除成功");
     }
-    
+
     /***
      * 多条件搜索品牌数据
      * @param searchMap
      * @return
      */
-    @GetMapping(value = "/search")
-    public Result findList(@RequestParam Map searchMap) {
+    @GetMapping(value = "/search" )
+    public Result findList(@RequestParam Map searchMap){
         List<Brand> list = brandService.findList(searchMap);
-        return new Result(true, StatusCode.OK, "查询成功", list);
+        return new Result(true,StatusCode.OK,"查询成功",list);
     }
-    
-    /***
-     * 分页搜索实现
-     * @param page
-     * @param size
-     * @return
-     */
-    @GetMapping(value = "/search/{page}/{size}")
-    public Result findPage(@PathVariable int page, @PathVariable int size) {
-        Page<Brand> pageList = brandService.findPage(page, size);
-        PageResult pageResult = new PageResult(pageList.getTotal(), pageList.getResult());
-        return new Result(true, StatusCode.OK, "查询成功", pageResult);
-    }
-    
+
+
     /***
      * 分页搜索实现
      * @param searchMap
@@ -107,5 +101,11 @@ public class BrandController {
         Page<Brand> pageList = brandService.findPage(searchMap, page, size);
         PageResult pageResult=new PageResult(pageList.getTotal(),pageList.getResult());
         return new Result(true,StatusCode.OK,"查询成功",pageResult);
+    }
+
+    @GetMapping("/category/{categoryName}")
+    public Result<List<Map>> findBrandListByCategoryName(@PathVariable("categoryName")String categoryName){
+        List<Map> brandList = brandService.findBrandListByCategoryName(categoryName);
+        return new Result<>(true,StatusCode.OK,"查询成功",brandList);
     }
 }
--- changgou_parent/changgou_service/changgou_service_goods/src/main/java/com/changgou/goods/service/BrandService.java
+++ changgou_parent/changgou_service/changgou_service_goods/src/main/java/com/changgou/goods/service/BrandService.java
@@ -7,46 +7,45 @@ import java.util.List;
 import java.util.Map;
 
 public interface BrandService {
-    
+
     /***
-     * 查询所有品牌
+     * 查询所有
      * @return
      */
     List<Brand> findAll();
-    
+
     /**
      * 根据ID查询
-     *
      * @param id
      * @return
      */
     Brand findById(Integer id);
-    
+
     /***
-     * 新增品牌
+     * 新增
      * @param brand
      */
     void add(Brand brand);
-    
+
     /***
-     * 修改品牌数据
+     * 修改
      * @param brand
      */
     void update(Brand brand);
-    
+
     /***
-     * 删除品牌
+     * 删除
      * @param id
      */
     void delete(Integer id);
-    
+
     /***
-     * 多条件搜索品牌方法
+     * 多条件搜索
      * @param searchMap
      * @return
      */
     List<Brand> findList(Map<String, Object> searchMap);
-    
+
     /***
      * 分页查询
      * @param page
@@ -54,7 +53,7 @@ public interface BrandService {
      * @return
      */
     Page<Brand> findPage(int page, int size);
-    
+
     /***
      * 多条件分页查询
      * @param searchMap
@@ -63,4 +62,13 @@ public interface BrandService {
      * @return
      */
     Page<Brand> findPage(Map<String, Object> searchMap, int page, int size);
+
+    /**
+     * 根据分类名称查询品牌列表
+     * @param categoryName
+     * @return
+     */
+    public List<Map> findBrandListByCategoryName(String categoryName);
+
+
 }
